c28de67880ee8bd2ae073f11068d5783d08ef5c7
Guido X Jansen <x@gui.do>
2024-12-11 10:50:13 +0100

    Build fixes

--- src/js/jsonLD.ts
+++ src/js/jsonLD.ts
@@ -13,10 +13,10 @@ interface GeneralProps {
 
 export interface BlogProps {
   type: "blog";
-  postFrontmatter: BlogData;
-  image: any; // result of getImage() from Seo.astro
-  authors: Author[];
-  canonicalUrl: URL;
+  postFrontmatter?: BlogData;
+  image?: any; // result of getImage() from Seo.astro
+  authors?: Author[];
+  canonicalUrl?: URL;
 }
 
 export type JsonLDProps = BlogProps | GeneralProps;
@@ -24,8 +24,12 @@ export type JsonLDProps = BlogProps | GeneralProps;
 export default function jsonLDGenerator(props: JsonLDProps) {
   const { type } = props;
   if (type === "blog") {
-    const { postFrontmatter, image, authors, canonicalUrl } =
+    const { postFrontmatter, image, authors = [], canonicalUrl } =
       props as BlogProps;
+
+    if (!postFrontmatter || !canonicalUrl) {
+      return '';
+    }
 
     let authorsJsonLdArray = authors.map((author) => {
       return {
@@ -39,33 +43,46 @@ export default function jsonLDGenerator(props: JsonLDProps) {
 
     if (authorsJsonLdArray.length === 1) {
       authorsJsonLd = authorsJsonLdArray[0];
-    } else {
+    } else if (authorsJsonLdArray.length > 1) {
       authorsJsonLd = authorsJsonLdArray;
+    } else {
+      authorsJsonLd = {
+        "@type": "Person",
+        name: siteData.author.name,
+        url: `https://twitter.com/${siteData.author.twitter}`,
+      };
+    }
+
+    const jsonLD = {
+      "@context": "https://schema.org",
+      "@type": "Blogposting",
+      "mainEntityOfPage": {
+        "@type": "WebPage",
+        "@id": canonicalUrl.toString()
+      },
+      "headline": postFrontmatter.title,
+      "description": postFrontmatter.description,
+      "author": authorsJsonLd,
+      "datePublished": postFrontmatter.pubDate,
+      "dateModified": postFrontmatter.updatedDate || postFrontmatter.pubDate
+    };
+
+    // Only add image if it exists and has a src property
+    if (image && typeof image === 'object' && 'src' in image) {
+      jsonLD["image"] = image.src;
     }
 
     return `<script type="application/ld+json">
-      {
-        "@context": "https://schema.org",
-        "@type": "Blogposting",
-        "mainEntityOfPage": {
-          "@type": "WebPage",
-          "@id": "${canonicalUrl}"
-        },
-        "headline": "${postFrontmatter.title}",
-        "description": "${postFrontmatter.description}",
-        "image": "${image.src}",
-        "author": ${JSON.stringify(authorsJsonLd)},
-        "datePublished": "${postFrontmatter.pubDate}",
-        "dateModified": "${postFrontmatter.updatedDate}"
-      }
+      ${JSON.stringify(jsonLD, null, 2)}
     </script>`;
   }
+
   return `<script type="application/ld+json">
-      {
+    {
       "@context": "https://schema.org/",
       "@type": "WebSite",
       "name": "${siteData.title}",
       "url": "${import.meta.env.SITE}"
-      }
-    </script>`;
+    }
+  </script>`;
 }
